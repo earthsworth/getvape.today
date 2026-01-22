7576f055a2e90533069d2a9526e36b471f0c8633
TianmuTNT <tianmutnt@qq.com>
2026-01-22 19:54:14 +0800

    feat: update Afdian link

--- src/config.js
+++ src/config.js
@@ -5,7 +5,8 @@ export const config = {
   // 卡网列表 (会自动随机排序)
   shops: [
     { name: "钟意卡网", url: "https://shop.zhongy.shop/item/86" },
-    { name: "Atri卡网", url: "https://shop.atri.cards/item?id=260" }
+    { name: "Atri卡网", url: "https://shop.atri.cards/item?id=260" },
+    { name: "爱发电", url: "https://afdian.com/item/2ef6a43cf78811f084b752540025c377" }
   ],
 
   // 建议零售价 (至多6个，超出可能会影响布局美观)
