b1e13ff15518b6e3a82322aa3f1d9493ebfc6727
TianmuTNT <tianmutnt@qq.com>
2026-01-24 02:51:57 +0800

    feat: add SL1me's Shop link

--- src/config.js
+++ src/config.js
@@ -6,7 +6,8 @@ export const config = {
   shops: [
     { name: "钟意卡网", url: "https://shop.zhongy.shop/item/86" },
     { name: "Atri卡网", url: "https://shop.atri.cards/item?id=260" },
-    { name: "爱发电", url: "https://afdian.com/item/2ef6a43cf78811f084b752540025c377" }
+    { name: "爱发电", url: "https://afdian.com/item/2ef6a43cf78811f084b752540025c377" },
+    { name: "SL1me's Shop", url:"https://shop.nekorua.com/" }
   ],
 
   // 建议零售价 (至多6个，超出可能会影响布局美观)
