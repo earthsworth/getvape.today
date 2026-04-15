dee59092456d6a26b0abf9c35fe0e4949797c094
TianmuTNT <tianmutnt@qq.com>
2026-04-16 01:23:56 +0800

    chore: remove 神人 & update NekoALTs

--- src/config.js
+++ src/config.js
@@ -1,20 +1,19 @@
 export const config = {
-  // 立即使用的跳转链接
-  useNowUrl: "https://relx.lunarclient.top/",
+    // 立即使用的跳转链接
+    useNowUrl: "https://relx.lunarclient.top/",
 
-  // 卡网列表 (会自动随机排序)
-  shops: [
-    { name: "钟意卡网", url: "https://shop.zhongy.shop/item/86" },
-    { name: "Atri卡网", url: "https://shop1.atrishop.autos/item?id=260" },
-    { name: "爱发电", url: "https://ifdian.net/item/2ef6a43cf78811f084b752540025c377" },
-    { name: "SL1me's Shop", url:"https://shop.nekorua.com/" },
-    { name: "RoseShop卡网", url:"https://roseshop.asia/item/83" }
-  ],
+    // 卡网列表 (会自动随机排序)
+    shops: [
+        { name: "钟意卡网", url: "https://shop.zhongy.shop/item/86" },
+        { name: "Atri卡网", url: "https://shop1.atrishop.autos/item?id=260" },
+        { name: "爱发电", url: "https://ifdian.net/item/2ef6a43cf78811f084b752540025c377" },
+        { name: "NekoALTs", url: "https://nekoalts.wtf/" },
+    ],
 
-  // 建议零售价 (至多6个，超出可能会影响布局美观)
-  msrp: [
-    { label: "3天卡", price: "￥2.00" },
-    { label: "周卡", price: "￥4.50" },
-    { label: "月卡", price: "￥15.00" },
-  ]
+    // 建议零售价 (至多6个，超出可能会影响布局美观)
+    msrp: [
+        { label: "3天卡", price: "￥2.00" },
+        { label: "周卡", price: "￥4.50" },
+        { label: "月卡", price: "￥15.00" },
+    ]
 };
